bddfb1bcab350693a2cd49a09f66900927bccc57
jay-seismic <jduffy@seismicgames.com>
2016-07-29 11:43:01 -0700

    update buzz timing

--- buzzerprotocol/src/main/java/com/seismicgames/jeopardyprototype/Constants.java
+++ buzzerprotocol/src/main/java/com/seismicgames/jeopardyprototype/Constants.java
@@ -4,7 +4,7 @@ package com.seismicgames.jeopardyprototype;
  * Created by jduffy on 7/6/16.
  */
 public class Constants {
-    public static final int BuzzInTimeout = 10000;
+    public static final int BuzzInTimeout = 5000;
     public static final int AnswerTimeout = 5000;
     public static final int AnswerListenTimeout = AnswerTimeout+1000;
     public static final int WagerTimeout = 15000;
